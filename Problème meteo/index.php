450ebccd81a2f252400ca52815dcde8cac526ef0
clelantoson <51384884+clelantoson@users.noreply.github.com>
2019-06-05 14:54:30 +0200

    add index.php in the folder + image background

---
+++ Problème meteo/index.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="fr" prefix="og: http://ogp.me/ns#">
+<head>
+	<title>Vol annulé : quels sont vos droits ? Loi, indemnités et réclamations</title>
+	<meta name="description" content="Le règlement Européen vous protège en cas d'annulation de vol. Découvrez quels sont les recours possibles et dans quelles conditions vous pouvez être indemnisé(e)">
+
+
+	<!-- Dynamic Meta Open Graph -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@Retard_Vol">
+	<meta name="twitter:image" content="http://www.retardvol.fr/assets/img/montant-indemnisation.jpg">
+	<meta name="twitter:image:alt" content="vol annulé vos droits">
+	<meta name="twitter:creator" content="@Retard_Vol">
+	<meta name="twitter:title" content="Quels sont vos droits en cas vol annulé ?">
+	<meta name="twitter:description" content="La compagnie a annulé votre vol à la dernière minute ? Tout savoir sur vos droits et les remboursements possibles.">
+	<meta name="twitter:domain" content="retardvol.fr">
+
+
+
+	<meta property="og:title" content=" Que faire en cas de vol annulé ? Tout savoir sur vos droits et votre éligibilité à une indemnisation." />
+	<meta property="og:url" content="https://retardvol.fr/vol-annule-vos-droits/" />
+	<meta property="og:description" content="Mon avion est annulé, quels sont mes droits ? Comment me faire indemniser ? Toutes les infos ici !" />
+	<meta property="og:type" content="website"/>
+
+	<meta property="og:image" content="https://retardvol.fr/assets/img/salle_1-min.png" />
+
+	<meta property="article:published_time" content="2019-05-29T12:00:56+00:00">
+	<meta property="article:modified_time" content="2019-05-29T12:00:56+00:00">
+	<meta property="og:updated_time" content="2019-06-02T12:00:56+00:00">
+
+	<link rel="stylesheet" href="https://retardvol.fr/styles/rtv.css">
+	<?php include('../head.html'); ?>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/solid.css">
+
+
+
+</head> 
+
+<body>
+	<?php include('../header.html'); ?>
+
+	<div class="parallax-container">
+		<div class="shadow-container">
+			<div class="center-align white-text pt-30">
+				<div class="container">
+					<h1>Vol annulé : vos droits</h1>
+					<a href="https://app.retardvol.fr/survey" target="_blank" class="waves-effect waves-light btn upper blue">vérifier mon vol</a>
+					<p>Jusqu'à 600€ d'indemnisation par passager</p>
+				</div>
+			</div>
+			<div class="parallax"><img src="https://retardvol.fr/assets/img/salle_1-min.png"></div>
+		</div>
+	</div>
+
+
+	<div class="droits-container">
+		<div class="textertv containerrtvlarge container">  
+
+			<br>
+
+			<p>L’objectif de l’Union européenne est de garantir une protection des passagers dans le domaine du transport aérien. Cette protection a été concrétisée avec l’adoption du réglement N°261/2004 du 11 février 2004 qui instaure des règles communes relatives à l’indemnisation et à l’assistance des passagers en cas d'annulation de vol.</p>
+
+
+			<h2>Montant de l'indemnité pour un vol annulé </h2>
+
+			<p>Le règlement Européen 261-2004 prévoit une indemnisation financière pour les passagers ayant subi un vol annulé. Cette indemnité peut être cumulable avec un éventuel remboursement du billet par la compagnie. </p>
+
+
+			<div class="mep-lu-il">
+				<ul>
+
+					<li><i class="fa fa-check" aria-hidden="true" style="color:green"></i><strong> 250 €</strong> par passager, pour les vols annulés dont la distance est de moins de 1.500 km</li>
+
+					<li><i class="fa fa-check" aria-hidden="true" style="color:green"></i><strong> 400 €</strong> par passager, pour les vols annulés dont la distance est entre 1.500km et 3.500km</li>
+
+					<li><i class="fa fa-check" aria-hidden="true" style="color:green"></i><strong> 400 €</strong> par passager, pour les vols de plus de 3.500km mais qui restent intra-communautaires (de métropole vers les DOM-TOM par exemple)</li>
+
+
+					<li><i class="fa fa-check" aria-hidden="true" style="color:green"></i><strong> 600 €</strong> par passager, pour les vols annulés dont la distance est de plus de 3.500km et non intra-communautaires (de Paris à New-York par exemple)</li>
+
+				</ul>
+			</div>
+
+
+			<h3>Attention ! Si la compagnie vous réachemine vers un autre vol et que vous arrivez à destination avec un retard "léger", les indemnisations sont diminuées de moitié :</h3>
+			<ul>
+				<li>
+					<i class="fa fa-check" aria-hidden="true" style="color:green"></i>
+					<strong>125€</strong> si vous avez un retard inférieur à <strong>2h</strong> à l'arrivée après réacheminement sur un vol inférieur à 1.500km
+				</li>
+				<li>
+					<i class="fa fa-check" aria-hidden="true" style="color:green"></i>
+					<strong>200€</strong> si vous avez un retard inférieur à <strong>3h</strong> à l'arrivée après réacheminement sur un vol. Valable pour les vols de plus de 1.500km intracommunautaires (=dans l'UE) ou entre 1.500km et 3.500km pour les autres vols 
+				</li>
+				<li>
+					<i class="fa fa-check" aria-hidden="true" style="color:green"></i>
+					<strong>300€</strong> si vous avez un retard inférieur à <strong>4h</strong> à l'arrivée après réacheminement sur un autre vol. Valable pour les vols supérieurs à 3.500km non intracommunautaires. (=en dehors de l'UE) 
+				</li>
+			</ul>
+
+
+
+
+			<p><a href="http://calculerlesdistances.com/aeriennes.html" target="_blank">Cliquez-ici</a> pour calculer une distance entre deux villes par avion.</p>
+
+			<img class="responsive-img" src="https://retardvol.fr/assets/img/montant-indemnisation.jpg" alt="indemnisation vol annulé" height="300">
+		</div>
+	</div>
+	<div class="band center-align">
+		<div class="container">
+			<h3>L'indemnité ne dépend pas du prix de votre billet !</h3>
+
+			<p>L'indemnisation financière est cumulable avec le remboursement de votre billet d'avion si la compagnie ne vous achemine pas au point d'arrivée prévu. Le montant de l'indemnité ne dépend en aucun cas du prix de votre billet. Vous pouvez donc toucher une indemnité plus ou moins élevée que le prix de votre voyage. Les billets business class ne sont pas davantage indemnisés qu'un billet en classe économie.
+			</p>
+		</div>
+	</div>  
+
+	<div class="container">
+
+		<h3>Quelles sont les cas éligibles à une indemnité sur un vol annulé ? </h3>
+
+
+		<div class="mep-lu-il">
+			<ul>  
+				<li><i class="fa fa-check" aria-hidden="true" style="color:green"></i> L'avion est annulé à cause d'un 
+					<a href=https://retardvol.fr/vos-droits-probleme-technique-avion/ target="_blank"><strong>problème technique</strong></a>
+
+				</li>
+				<li><i class="fa fa-check" aria-hidden="true" style="color:green"></i> L'avion est annulé à cause d'un problème avec <strong>l'équipage</strong> (temps de vol dépassé, pilote malade, ...)
+				</li>
+				<li><i class="fa fa-check" aria-hidden="true" style="color:green"></i> L'avion est annulé car <a href=https://retardvol.fr/greve-avion-indemnisation/ target="_blank"><strong>la compagnie fait grève</strong></a> (voir nos pages <a href="https://retardvol.fr/greve-air-france-que-faire/">grève Air France</a> et <a href="https://retardvol.fr/voyage/refus-indemnisation-greve-ryanair/">grève Ryanair</a>)
+				</li>
+
+				<li><i class="fa fa-check" aria-hidden="true" style="color:green"></i> <strong>Aucune raison</strong> ne vous est communiquée par la compagnie
+				</li>
+
+				<li><i class="fa fa-check" aria-hidden="true" style="color:green"></i> <strong> Votre vol date d'il y a 5 ans ou moins</strong> : vous avez le temps pour faire votre demande d'indemnisation, mais ne tardez pas 
+				</li>
+
+			</ul>
+		</div>
+	</div>
+
+
+	<div class="container">
+
+		<h3>Dans quels cas n'est-on pas éligible à une indemnisation ? </h3>
+
+
+		<div class="mep-lu-il">
+			<ul> 
+
+				<li><i class="fa fa-times" aria-hidden="true" style="color:red"></i>  La compagnie a annulé le vol ou changé les horaires en <strong>vous prévenant plus de 14 jours avant la date du vol</strong>
+				</li>
+				<li><i class="fa fa-times" aria-hidden="true" style="color:red"></i> L'annumation est due à un <strong>problème météo</strong>. <br>
+					⚠️ Attention cependant, les compagnies évoquent parfois <strong>des problèmes météo qui n'en sont en pas</strong>. Soyez insistant sur les raisons de l'annulation. Notre équipe vérifie les METAR (données météo aéronautiques) pour savoir si c'est réellement la météo qui est la cause de l'annulation. Les causes fréquentes d'annulation : cyclones, rafales de vent, orages, neige abondante.
+
+				</li>
+				<li><i class="fa fa-times" aria-hidden="true" style="color:red"></i> Le vol est annulé à cause de <a href=https://retardvol.fr/voyage/greve-controle-aerien/ target="_blank"><strong>la grève des contrôleurs aériens</strong></a>
+
+				</li>
+				<li><i class="fa fa-times" aria-hidden="true" style="color:red"></i> L'aéroport de départ n'est pas dans l'union Européenne <b>ET</b> la compagnie n'est pas Européenne
+					</li>
+
+					<li><i class="fa fa-times" aria-hidden="true" style="color:red"></i> Le vol est en retard à cause d'une circonstance extraordinaire non imputable à la compagnie. Attention toutefois il existe une jurisprudence fournie sur le sujet. Consultez-nous pour éluder vos doutes.
+				</li>
+				<li><i class="fa fa-times" aria-hidden="true" style="color:red"></i> Un passager ou un membre de l'équipage est <strong>malade</strong>
+				</li>
+				<li><i class="fa fa-times" aria-hidden="true" style="color:red"></i> L'avion a été <strong>touché par la foudre</strong> et doit être inspecté
+				</li>
+				<li><i class="fa fa-times" aria-hidden="true" style="color:red"></i> L'avion a eu <strong>une collision avec un oiseau</strong> et doit être inspecté
+				</li>
+				<li><i class="fa fa-times" aria-hidden="true" style="color:red"></i> Le vol précédent votre vol a été retardé à cause d'une circonstance extraordinaire (météo, grève des contrôleurs, ...) et ce retard a causé un retard sur votre vol (si par contre le retard du vol précédent a été causé par un problème technique, vous êtes bien éligible à une indemnité)
+				</li>
+
+			</ul>
+		</div>
+	</div>
+
+
+	<br>
+
+
+	<div class="container">
+
+		<h2>Peut-on être indemnisé si le vol n'était pas effectué par une compagnie Européenne ? </h2>
+
+		<div class="row">
+			<div class="col s12 m6">
+				<ul>
+					<li><h3>Vous voyagez avec une compagnie Européenne</h3></li>
+					<li>
+						<i class="fa fa-check" aria-hidden="true" style="color:green"></i>
+						Le départ et l'arrivée sont dans l'UE
+					</li>
+					<li>
+						<i class="fa fa-check" aria-hidden="true" style="color:green"></i>
+						Seule l'arrivée est dans l'UE
+					</li>
+					<li>
+						<i class="fa fa-check" aria-hidden="true" style="color:green"></i>
+						Seul le départ est dans l'UE
+					</li>
+					<li>
+						<i class="fa fa-times" aria-hidden="true" style="color:red"></i> 
+						Ni le départ ni l'arrivée ne sont dans l'UE
+					</li>
+				</ul>
+			</div>
+
+			<div class="col s12 m6">
+				<ul>
+					<li><h3>La compagnie n'est pas Européenne</h3></li>
+					<li>
+						<i class="fa fa-check" aria-hidden="true" style="color:green"></i>
+						Le départ et l'arrivée sont dans l'UE
+					</li>
+					<li>
+						<i class="fa fa-check" aria-hidden="true" style="color:green"></i>
+						Seul le départ est dans l'UE
+					</li>
+					<li>
+						<i class="fa fa-times" aria-hidden="true" style="color:red"></i> 
+						Seule l'arrivée est dans l'UE
+					</li>
+					<li>
+						<i class="fa fa-times" aria-hidden="true" style="color:red"></i> 
+						Ni le départ ni l'arrivée ne sont dans l'UE
+					</li>
+
+				</ul>
+
+			</div>
+
+		</div>
+
+
+
+		<p class="note">
+			<span class="font-bold">Remarque :</span> L’UE comprend les 28 pays de l’Union européenne, l’Islande, la Norvège, le Royaume-Uni, la Suisse et y compris la Guadeloupe, la Guyane française, la Réunion, Mayotte, Saint-Martin, les Açores, Madère, les Îles Canaries et la Martinique. En revanche ne sont pas compris les Îles Féroé, l’Île de Mans et les Îles Anglo-Normandes.
+		</p>
+	</div>
+</div>
+
+
+<div class="container">
+
+	<h2>Comment obtenir son indemnisation ?</h2>
+	<p>La prise de contact avec la compagnie n'est généralement <span class="font-bold">pas facile</span> : nombreuses heures d'attente au téléphone, difficulté à trouver les pages de réclamation, longueur à répondre, etc ...</p>
+	<p>Il est préférable d'envoyer directement les éléments de droit du <span class="font-bold">règlement 261-2004</span> et de faire savoir aux compagnies que vous connaissez ce règlement et leur obligation à vous indemniser.</p>
+	<p>Il est très fréquent que les compagnies proposent des bons d'achats pour de prochains vols. Sachez que rien ne vous oblige à accepter le bon d'achat, vous pouvez demander une indemnisation en numéraire.</p>
+	<p>Il est aussi très fréquent que les montants proposés par les compagnies soient <span class="font-bold">très en dessous</span> des montants d'indemnisation auxquels vous avez droit. Attention donc à bien calculer le montant de votre indemnisation sur votre vol annulé.</p>
+	<p>Enfin, certaines compagnies peu scrupuleuses évoqueront des circonstances exceptionnelles afin de se dédouaner de la responsabilité du retard de votre vol. Une bonne connaissance de la jurisprudence est alors nécessaire pour contrer lorsque c'est possible les arguments des compagnies dont le vol est annulé.</p>
+
+	<p><strong>Pour simplifier vos démarches et gagner du temps</strong>: vous pouvez faire appel à RetardVol. Notre équipe de juristes et avocats experts en droit aérien s'occuppent des procédures pour vous et vous obtiendrez l'indemnité maximale à laquelle vous avez le droit. Si jamais les compagnies aériennes ne coopèrent pas nous les assigons en justice gratuitement pour vous avec nos avocats.</p>
+</div>
+
+<div class="band center-align">
+	<div class="container">
+		<h3>Jusqu'à 600€ d'indemnisation par passager</h3>
+		<br>
+		<a href="https://app.retardvol.fr/survey" target="_blank" class="waves-effect waves-light btn upper blue">vérifier mon vol</a>
+		<p>
+			Nous vérifions dans un premier temps l'éligibilité de votre vol à une indemnisation. Nous vous
+			demanderons ensuite les pièces pour constituer votre dossier. Vous n'aurez ensuite plus qu'à attendre
+			la bonne nouvelle de votre indemnisation.
+		</p>
+	</div>
+</div>
+
+<br> <br>
+
+
+
+<div class="container">
+	<h3>D'autres pages qui peuvent vous intéresser : </h3>
+	<div class="blockInfoContainer" id="blockInfoContainerId">
+		<div class="blockInfoImageContainer">
+			<img src="https://retardvol.fr/assets/img/imgcarree.jpg" alt="Droits vol annulé">
+		</div>
+
+		<div class="blockInfoRight">
+			<div class="blockInfoTitle">
+				Vos droits en cas de vol retardé
+			</div>
+			<p>Votre vol peut être retardé pour diverses raisons. Si cela vous arrive, sauf circonstance extraordinaire, vous avez droit à une indemnisation comprise entre 250€ et 600€.  <br><a href="https://retardvol.fr/vol-retarde-vos-droits/" target="_blank" class="waves-effect waves-light btn upper blue">Lire la suite</a></p>
+		</div>
+	</div>
+</div>
+
+
+<div class="container">
+	<div class="blockInfoContainer" id="blockInfoContainerId">
+		<div class="blockInfoImageContainer">
+			<img src="https://retardvol.fr/assets/img/imgcarreesurbooking.jpg" alt="Droits surbooking">
+		</div>
+		<div class="blockInfoRight">
+			<div class="blockInfoTitle">
+				Vos droits en cas de surbooking
+			</div>
+			<p>La compagnie a refusé votre embarquement sur le vol que vous aviez prévu. Si cela vous arrive, et dans le cas où vous ne faisiez pas partie des volontaires pour prendre un prochain vol, vous avez droit à une indemnisation comprise entre 250€ et 600€. <br><a href="https://retardvol.fr/surbooking-tout-savoir/" target="_blank" class="waves-effect waves-light btn upper blue">Lire la suite</a></p>
+		</div>
+	</div>
+</div>
+
+<br>
+
+<div class="container">
+	<div class="blockInfoContainer" id="blockInfoContainerId">
+		<div class="blockInfoImageContainer">
+			<img src="https://retardvol.fr/assets/img/imgcarreecmanquee.jpg" alt="Droits correspondance manquée">
+		</div>
+		<div class="blockInfoRight">
+			<div class="blockInfoTitle">
+			Vos droits en cas de correspondance manquée</div>
+
+			<p>Vous avez raté votre vol lors d'une escale car votre premier vol était annulé ou en retard. Avez-vous le droit à une indemnisation ? Cela dépend des cas, notamment si vous avez réservé les deux vols ensemble ou non. <br><a href="https://retardvol.fr/correspondance-manquee-remboursement-indemnisation/" target="_blank" class="waves-effect waves-light btn upper blue">Lire la suite</a></p>
+		</div>
+	</div>
+</div> 
+
+</div>
+</div>
+
+<?php include('../footer.html'); ?>
+
+</body>
+</html>
